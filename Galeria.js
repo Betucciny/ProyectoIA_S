e638f44d7b81d80e0e955acfa86ede01489ad043
Roberto Herrera <betohr48@hotmail.com>
2022-12-07 16:49:23 -0600

    cambios js funcionando

---
+++ Galeria.js
@@ -0,0 +1,67 @@
+class Galeria{
+    constructor(src, contenedor) {
+        this.src = src;
+        this.contenedor = contenedor;
+        this.currentIndex = 0;
+        this.buttonL = document.createElement('button');
+        this.buttonL.dataset.index = "0";
+        this.buttonL.innerHTML =
+            '<svg width="70px" height="80px" viewBox="0 0 50 80">\n' +
+            '                <polyline fill="none" stroke="#000000" stroke-width="1" stroke-linecap="round"\n' +
+            '                          stroke-linejoin="round" points="\n' +
+            '\t                        45.63,75.8 0.375,38.087 45.63,0.375 "/>\n' +
+            '            </svg>\n'
+        this.buttonR = document.createElement('button');
+        this.buttonL.dataset.index = "1";
+        this.buttonR.innerHTML = '' +
+            '            <svg width="70px" height="80px" viewBox="0 0 50 80">\n' +
+            '                <polyline fill="none" stroke="#000000" stroke-width="1" stroke-linecap="round"\n' +
+            '                          stroke-linejoin="round" points="\n' +
+            '\t                           0.375,0.375 45.63,38.087 0.375,75.8 "/>\n' +
+            '            </svg>'
+        this.images = [];
+        for(let i=0; i<this.src.length; i++){
+            const imagen = document.createElement('img');
+            imagen.src = src[i];
+            this.images.push(imagen);
+        }
+        this.current = this.images[0];
+        this.contenedor.appendChild(this.buttonL)
+        this.contenedor.appendChild(this.current)
+        this.contenedor.appendChild(this.buttonR)
+        this.nextImage = this.nextImage.bind(this);
+        this.buttonR.addEventListener('click', this.nextImage);
+        this.preImage = this.preImage.bind(this);
+        this.buttonL.addEventListener('click', this.preImage);
+    }
+
+    nextImage(){
+        this.currentIndex ++;
+        if (this.currentIndex === this.images.length){
+            this.currentIndex = 0
+        }
+        this.current = this.images[this.currentIndex];
+        this.contenedor.childNodes[2].replaceWith(this.current);
+    }
+
+    preImage(){
+        this.currentIndex --;
+        if (this.currentIndex < 0){
+            this.currentIndex = this.images.length-1;
+        }
+        this.current = this.images[this.currentIndex];
+        this.contenedor.childNodes[2].replaceWith(this.current);
+    }
+
+}
+
+const REGALOS_ENLACES = [
+    'https://media.giphy.com/media/27ppQUOxe7KlG/giphy.gif',
+    'https://media.giphy.com/media/LEcRaYyUptIxG/giphy.gif',
+    'https://media.giphy.com/media/3BBv1D4AFbJkY/giphy.gif',
+    'https://media.giphy.com/media/13smkcXZiTLDgc/giphy.gif',
+    'https://media.giphy.com/media/GknfGjiYhsFQk/giphy.gif'
+];
+
+const cont = document.querySelector('#datos')
+const galeria = new Galeria(REGALOS_ENLACES, cont)
